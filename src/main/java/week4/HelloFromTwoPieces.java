e8af7ea100f1f1c67818a967795ae6662d9917ad
valyi-sandor-nye <urugvalyi@gmail.com>
2024-10-02 17:45:32 +0200

    week4 package is added HelloFromTwoPieces.java is added

---
+++ src/main/java/week4/HelloFromTwoPieces.java
@@ -0,0 +1,16 @@
+package week4;
+
+public class HelloFromTwoPieces {
+    public static void main(String[] args) {
+        System.out.println(a()+b());
+    }
+
+
+    private static String a() {
+        return "a";
+    }
+
+    private static String b() {
+        return "b";
+    }
+}
